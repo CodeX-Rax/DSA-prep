6502beef0c31f88efc741331cf709d7faf55d4f2
Rushabh Chamat <rushabhchamat2525@gmail.com>
2025-03-26 01:23:32 +0530

    commit

--- src/TRY.java
+++ src/TRY.java
@@ -1,10 +1,22 @@
-import java.util.Scanner;
-
 public class TRY {
+    public static boolean Armstrong(int num){
+        int n=num;
+        int k=String.valueOf(num).length();
+        int sum=0;
+        while(n>0){
+            int ld=n%10;
+            sum+=Math.pow(ld,k);
+            n=n/10;
+        }
+        return sum==num ? true:false;
+
+    }
     public static void main(String[] args) {
-        int a=15;
-        int b=20;
-        int res=a%b;
-        System.out.println(res);
+        int n=186;
+        if(Armstrong(n)){
+            System.out.println("yes");
+        }else{
+            System.out.println("no");
+        }
     }
 }
