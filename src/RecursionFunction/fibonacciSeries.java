4bd6ee7547b6087e5c39de74a20a6e5f6bd21fed
Rushabh Chamat <rushabhchamat2525@gmail.com>
2025-03-27 23:42:34 +0530

    commit

---
+++ src/RecursionFunction/fibonacciSeries.java
@@ -0,0 +1,29 @@
+package RecursionFunction;
+
+import java.util.Scanner;
+
+public class fibonacciSeries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Ask user for the number of terms in the Fibonacci series
+        System.out.print("Enter the number of terms: ");
+        int terms = sc.nextInt();
+
+        // First two terms of the Fibonacci series
+        int firstTerm = 0, secondTerm = 1;
+
+        System.out.println("Fibonacci Series up to " + terms + " terms:");
+
+        for (int i = 1; i <= terms; i++) {
+            System.out.print(firstTerm + " ");
+
+            // Calculate the next term by summing the previous two
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
+
+        //sc.close();
+    }
+}
